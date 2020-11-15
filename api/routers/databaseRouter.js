f88e0b0c89859a1023c9935eae06f2af5707258e
Patrick Zabel <patzab@web.de>
2020-11-15 20:22:29 +0100

    added database read router

--- api/routers/databaseRouter.js
+++ api/routers/databaseRouter.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const databaseRouter = express.Router();
+const DBReadRouter = require('./DBReadRouter')
+
+databaseRouter.use('/read', DBReadRouter);
 
 databaseRouter.get('/', (req, res, next) => {
     console.log('DATABASE TEST');
 })
+
 
 module.exports = databaseRouter;
 
